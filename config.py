d4150da8934f9bb267c8d2aae6623d4f1196b4bd
Duncan Warukira <dwarukira@gmail.com>
2018-04-21 02:09:12 +0300

    secrect

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
     ASTUSERNAME = 'sandbox                                                                                                                                                                                                                                                                                                                                                                                                                      '
     ASTAPI_KEY = '3d9ca64e8d14a82d2a7afbfebd6189c99c297adbade83cd088eb31ce0ee27876'
-
+    secret_key
+    SECRET_KEY = "\x05y\xfc\xa3\xa5T\x07\xa7\x82\xdb\xe5-mK5\xebZl\xe5\xe3\xce[\x1bT"
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(BASE_DIR, 'app.db')
