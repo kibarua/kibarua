2c0b554bdbccf34c958c5732dcdc1a113c531ee6
Duncan Warukira <dwarukira@gmail.com>
2018-04-21 06:46:59 +0300

    all

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Config:
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-    ASTUSERNAME = 'sandbox                                                                                                                                                                                                                                                                                                                                                                                                                      '
-    ASTAPI_KEY = '33115a3b3e74dffac8476b04d6ec6d7fa547c7502eef8d24968f8bd23d07f31c'
+    ASTUSERNAME = "sandbox"
+    ASTAPI_KEY = "33115a3b3e74dffac8476b04d6ec6d7fa547c7502eef8d24968f8bd23d07f31c"
     SECRET_KEY = "\x05y\xfc\xa3\xa5T\x07\xa7\x82\xdb\xe5-mK5\xebZl\xe5\xe3\xce[\x1bT"
 class DevConfig(Config):
     DEBUG = True
