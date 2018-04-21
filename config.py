908a29d901e43a75a37c1c10aabbfbc9b61dd162
Duncan Warukira <dwarukira@gmail.com>
2018-04-21 16:21:12 +0300

    cate lass 2

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Config:
     BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-    ASTUSERNAME = "Dun"
+    ASTUSERNAME = "Kibarua"
     ASTAPI_KEY = "4506e82b61eeb95fbb51c4d8ea123cdcd55f72a1f8836effd2c32974476948fc"
     SECRET_KEY = "\x05y\xfc\xa3\xa5T\x07\xa7\x82\xdb\xe5-mK5\xebZl\xe5\xe3\xce[\x1bT"
 class DevConfig(Config):
